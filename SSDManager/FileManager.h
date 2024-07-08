5b2c7a51be5573a4af8df638c067d43304d55f9f
jinho <zazz0907@naver.com>
2024-07-08 16:20:22 +0900

    [feat] Update FileManager class skeleton code and its unit test

--- SSDManager/FileManager.h
+++ SSDManager/FileManager.h
@@ -1,12 +1,15 @@
 #pragma once
 #include <iostream>
 #include <fstream>
+#include <string>
 
 class FileManager
 {
 public:
 	FileManager();
 	std::fstream* open(std::string);
+	std::fstream* init(std::string name);
+	unsigned int read(std::fstream* fs, int index);
 	bool write(std::fstream* fs, int index, unsigned int value);
 	bool write(std::fstream* fs, unsigned int value);
 	bool close(std::fstream* fs);
--- SSDManagerTest/FileManagerTest.cpp
+++ SSDManagerTest/FileManagerTest.cpp
@@ -1,10 +1,42 @@
+/* Copyright 2024 Code Love you */
+
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
 #include "../SSDManager/FileManager.cpp"
 
-using namespace testing;
-TEST(FileManagerTest, file_manager_test_00) 
-{
-	FileManager fm;
-	EXPECT_THAT(fm.open("test.txt"), IsNull());
+#define TEST_INDEX  (0)
+#define TEST_VALUE  (0xAAAAAAAA)
+
+TEST(FileManagerTest, file_manager_test_open) {
+    FileManager fm;
+    EXPECT_THAT(fm.open("test.txt"), testing::IsNull());
+}
+
+TEST(FileManagerTest, file_manager_test_init) {
+    FileManager fm;
+    EXPECT_THAT(fm.init("test.txt"), testing::IsNull());
+}
+
+TEST(FileManagerTest, file_manager_test_read) {
+    FileManager fm;
+    std::fstream * fs = fm.open("test.txt");
+    EXPECT_THAT(fm.read(fs, TEST_INDEX), 0);
+}
+
+TEST(FileManagerTest, file_manager_test_write) {
+    FileManager fm;
+    std::fstream* fs = fm.open("test.txt");
+    EXPECT_THAT(fm.write(fs, TEST_INDEX, TEST_VALUE), true);
+}
+
+TEST(FileManagerTest, file_manager_test_write_during_read) {
+    FileManager fm;
+    std::fstream* fs = fm.open("test.txt");
+    EXPECT_THAT(fm.write(fs, TEST_VALUE), true);
+}
+
+TEST(FileManagerTest, file_manager_test_close) {
+    FileManager fm;
+    std::fstream* fs = fm.open("test.txt");
+    EXPECT_THAT(fm.close(fs), true);
 }
