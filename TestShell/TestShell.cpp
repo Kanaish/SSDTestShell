5d64e422a4ad49ee61cfb6fabf11bf56c54fcbad
Jongin Kim <k.jongin@gmail.com>
2024-07-09 13:41:36 +0900

    [feat] implement write function

--- TestShell/TestShell.cpp
+++ TestShell/TestShell.cpp
@@ -1,6 +1,11 @@
 /* Copyright 2024 Code Love you */
 
 #include <iostream>
+#include <sstream>
+#include <cstring>
+#include <cstdlib>
+#include <regex>
+#include <stdexcept>
 
 #include "TestShell.h"
 
@@ -9,6 +14,49 @@ void TestShell::run(void) {
 
 bool TestShell::isValidCommand(const std::string& cmd) {
   return commandMap.find(cmd) != commandMap.end();
+}
+
+bool TestShell::isValidArgument(const std::string& arg) {
+  std::istringstream iss(arg);
+  std::string first_word, second_word, third_word;
+
+  iss >> first_word >> second_word >> third_word;
+
+  if (!third_word.empty()) {
+    return false;
+  }
+
+  try {
+    int number = std::stoi(first_word);
+    if (number < 0 || number > 99) {
+      return false;
+    }
+  }
+  catch (std::invalid_argument const& e) {
+    return false;
+  }
+  catch (std::out_of_range const& e) {
+    return false;
+  }
+
+  if (second_word.size() != 10 || second_word.substr(0, 2) != "0x") {
+    return false;
+  }
+  try {
+    size_t pos;
+    unsigned long value = std::stoul(second_word.substr(2), &pos, 16);
+    if (pos != 8) {
+      return false;
+    }
+  }
+  catch (std::invalid_argument const& e) {
+    return false;
+  }
+  catch (std::out_of_range const& e) {
+    return false;
+  }
+
+  return true;
 }
 
 void TestShell::execute(std::string input_str) {
@@ -30,15 +78,24 @@ void TestShell::execute(std::string input_str) {
     return;
   }
 
-  std::cout << "Invalid command: " << cmd << std::endl;
+  throw std::invalid_argument("INVALID COMMAND");
 }
 
-bool TestShell::write(const std::string& arg) {
-  return true;
+int TestShell::write(const std::string& arg) {
+  std::string cmd = "ssd.exe w ";
+
+  if (isValidArgument(arg)) {
+    cmd += arg;
+    std::cout << cmd << std::endl;
+    return system(cmd.c_str());
+  }
+
+  throw std::invalid_argument("INVALID COMMAND");
+  return -1;
 }
 
-bool TestShell::read(const std::string& arg) {
-  return true;
+int TestShell::read(const std::string& arg) {
+  return 0;
 }
 
 void TestShell::exit(void) {
@@ -47,10 +104,10 @@ void TestShell::exit(void) {
 void TestShell::help(void) {
 }
 
-bool TestShell::fullWrite(const std::string& arg) {
-  return true;
+int TestShell::fullWrite(const std::string& arg) {
+  return 0;
 }
 
-bool TestShell::fullRead(void) {
-  return true;
+int TestShell::fullRead(void) {
+  return 0;
 }
--- TestShell/TestShell.h
+++ TestShell/TestShell.h
@@ -9,10 +9,10 @@ class TestShell {
  public:
   void run(void);
   void execute(const std::string inputStr);
-  virtual bool write(const std::string& arg);
-  virtual bool read(const std::string& arg);
-  virtual bool fullWrite(const std::string& arg);
-  virtual bool fullRead(void);
+  virtual int write(const std::string& arg);
+  virtual int read(const std::string& arg);
+  virtual int fullWrite(const std::string& arg);
+  virtual int fullRead(void);
 
  protected:
   virtual void exit(void);
@@ -20,6 +20,7 @@ class TestShell {
 
  private:
   bool isValidCommand(const std::string& cmd);
+  bool isValidArgument(const std::string& arg);
 
   const std::unordered_map<std::string,
       std::function<void(const std::string&)>> commandMap = {
