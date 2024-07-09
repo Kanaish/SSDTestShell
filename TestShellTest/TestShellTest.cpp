cf211a180f9d7cb266295a31c2d0a5d3909ba799
jiyeon <eren08@naver.com>
2024-07-09 10:22:04 +0900

    [fix] fix argument

--- TestShellTest/TestShellTest.cpp
+++ TestShellTest/TestShellTest.cpp
@@ -21,13 +21,13 @@ TEST(ShellTest, execute_test_invalid_command) {
 
 TEST(ShellTest, execute_test_invalid_argument1) {
   TestShell shell;
-  std::string str{ "read 3 0xAAAABBBB00" };
+  std::string str{ "write 3 0xAAAABBBB00" };
   EXPECT_THROW(shell.execute(str), runtime_error);
 }
 
 TEST(ShellTest, execute_test_invalid_argument2) {
   TestShell shell;
-  std::string str{ "read 100 0xAAAABBBB" };
+  std::string str{ "read 100" };
   EXPECT_THROW(shell.execute(str), runtime_error);
 }
 
