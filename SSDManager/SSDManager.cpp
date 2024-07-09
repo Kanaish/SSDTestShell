cc37d239f2c3d4b93d367744ad76bcd47a9f7587
bsjn16sw <bsjn16sw@gmail.com>
2024-07-09 10:05:17 +0900

    [feat] validate input string

--- SSDManager/SSDManager.cpp
+++ SSDManager/SSDManager.cpp
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <stdexcept>
 
 class SSDManager {
  public:
@@ -9,6 +10,57 @@ class SSDManager {
         for (int i = 0; i < argc; i++) {
             parsed_command.push_back(argv[i]);
         }
+    }
+
+    bool isValidInput() {
+        int argc = parsed_command.size();
+        if (argc < 2) {
+            return false;
+        }
+
+        std::string& cmd = parsed_command[1];
+        if (cmd != "W" && cmd != "R") {
+            return false;
+        }
+
+        if (argc < 3) {
+            return false;
+        }
+
+        std::string& index = parsed_command[2];
+        try {
+            if (stoi(index) < 0 || stoi(index) > 99) {
+                return false;
+            }
+        }
+        catch (std::exception& e) {  // stoi() fail
+            return false;
+        }
+
+        if (cmd == "W") {
+            if (argc != 4) {
+                return false;
+            }
+
+            std::string& value = parsed_command[3];
+            if (value.length() != 10) {
+                return false;
+            }
+            if (value.substr(0, 2) != "0x") {
+                return false;
+            }
+            for (char& c : value.substr(2, 8)) {
+                if (c < '0' || (c > '9' && c < 'A') || c > 'F') {
+                    return false;
+                }
+            }
+        }
+
+        if (cmd == "R" && argc != 3) {
+            return false;
+        }
+
+        return true;
     }
 
     std::vector<std::string> getParsedCommand() {
--- SSDManagerTest/SSDManagerTest.cpp
+++ SSDManagerTest/SSDManagerTest.cpp
@@ -18,3 +18,103 @@ TEST(SSDManagerTest, constructorTest) {
     // Assert
     EXPECT_EQ(ssd.getParsedCommand(), expected);
 }
+
+TEST(SSDManagerTest, validOkayTest) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "2", "0x1298CDEF" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), true);
+}
+
+TEST(SSDManagerTest, validFailTest1) {
+    // Arrange
+    int argc = 1;
+    char* argv[] = { "ssd" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest2) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "Write", "2", "0x1298CDEF" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest3) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "ABC", "0x1298CDEF" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest4) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "100", "0x1298CDEF" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest5) {
+    // Arrange
+    int argc = 3;
+    char* argv[] = { "ssd", "W", "2" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest6) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "2", "0x1234ABCD1234ABCD"};
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTest7) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "2", "1234ABCD1234ABCD" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTes8) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "W", "2", "0xABCDEFGH" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
+
+TEST(SSDManagerTest, validFailTes9) {
+    // Arrange
+    int argc = 4;
+    char* argv[] = { "ssd", "R", "2", "0xABCDEFGH" };
+    SSDManager ssd(argc, argv);
+
+    // Act, Assert
+    EXPECT_EQ(ssd.isValidInput(), false);
+}
