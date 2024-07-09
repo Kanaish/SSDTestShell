1e664d99806f08f49d8437e0f42afada5a53879d
jiyeon <eren08@naver.com>
2024-07-09 10:01:25 +0900

    [style] apply cpplint

--- TestShellTest/TestShellTest.cpp
+++ TestShellTest/TestShellTest.cpp
@@ -6,25 +6,19 @@
 using namespace std;
 
 TEST(ShellTest, execute_test_invalid_command) {
-	TestShell shell;
-	std::string str{ "writ 3 0xAAAABBBB" };
-	EXPECT_THROW(shell.execute(str), runtime_error);
+  TestShell shell;
+  std::string str{ "writ 3 0xAAAABBBB" };
+  EXPECT_THROW(shell.execute(str), runtime_error);
 }
 
 TEST(ShellTest, execute_test_invalid_argument1) {
-	TestShell shell;
-	std::string str{ "read 3 0xAAAABBBB00" };
-	EXPECT_THROW(shell.execute(str), runtime_error);
-}
-
-TEST(ShellTest, execute_test_invalid_argument) {
-	string str{ "read 3 0xAAAABBB0CC" };
-	TestShell shell;
-	EXPECT_THROW(shell.execute(str), runtime_error);
+  TestShell shell;
+  std::string str{ "read 3 0xAAAABBBB00" };
+  EXPECT_THROW(shell.execute(str), runtime_error);
 }
 
 TEST(ShellTest, execute_test_invalid_argument2) {
-	TestShell shell;
-	std::string str{ "read 100 0xAAAABBBB" };
-	EXPECT_THROW(shell.execute(str), runtime_error);
+  TestShell shell;
+  std::string str{ "read 100 0xAAAABBBB" };
+  EXPECT_THROW(shell.execute(str), runtime_error);
 }
