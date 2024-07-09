7253b75e40051cb502639c6ddf82527f693ffe51
bsjn16sw <bsjn16sw@gmail.com>
2024-07-09 15:11:09 +0900

    [feat] introduce FileManager, SSDWriter, SSDReader (in comment)

--- SSDManager/SSDManager.cpp
+++ SSDManager/SSDManager.cpp
@@ -3,6 +3,9 @@
 #include <string>
 #include <vector>
 #include <stdexcept>
+//#include "FileManager.h"
+//#include "SSDWriter.h"
+//#include "SSDReader.h"
 
 class SSDManager {
  public:
@@ -10,7 +13,18 @@ class SSDManager {
         for (int i = 0; i < argc; i++) {
             parsed_input.push_back(argv[i]);
         }
+
+        //// TODO: Factory Pattern
+        //file_manager = new FileManager();
+        //ssd_writer = new SSDWriter(file_manager);
+        //ssd_reader = new SSDReader(file_manager);
     }
+
+    //~SSDManager() {
+    //    delete file_manager;
+    //    delete ssd_writer;
+    //    delete ssd_reader;
+    //}
 
     bool isValidInput() {
         int argc = parsed_input.size();
@@ -34,12 +48,38 @@ class SSDManager {
         return true;
     }
 
+    //bool executeCommand() {
+    //    if (isValidInput() == false) {
+    //        return false;
+    //    }
+
+    //    std::string& cmd = parsed_input[1];
+    //    int index = convertIndexInt();
+
+    //    if (cmd == "R") {
+    //        return ssd_reader.read(NAND_FILE, RESULT_FILE, index);
+    //    }
+
+    //    if (cmd == "W") {
+    //        std::string& value = parsed_input[3];
+    //        return ssd_writer.write(NAND_FILE, index, value);
+    //    }
+    //}
+
     std::vector<std::string> getParsedCommand() {
         return parsed_input;
     }
 
+
  private:
     std::vector<std::string> parsed_input;
+
+    //FileManager* file_manager;
+    //SSDWriter* ssd_writer;
+    //SSDReader* ssd_reader;
+
+    const std::string NAND_FILE = "nand.txt";
+    const std::string RESULT_FILE = "result.txt";
 
     bool isValidCommand(int argc) {
         if (argc < 2) {
@@ -73,7 +113,7 @@ class SSDManager {
     }
 
     bool isValidReadInput(int argc) {
-        std::string cmd = parsed_input[1];
+        std::string& cmd = parsed_input[1];
 
         if (cmd == "R" && argc != 3) {
             return false;
@@ -83,7 +123,7 @@ class SSDManager {
     }
 
     bool isValidWriteInput(int argc) {
-        std::string cmd = parsed_input[1];
+        std::string& cmd = parsed_input[1];
 
         if (cmd == "W") {
             if (argc != 4) {
@@ -105,5 +145,9 @@ class SSDManager {
         }
 
         return true;
+    }
+
+    int convertIndexInt() {
+        return stoi(parsed_input[2]);
     }
 };
