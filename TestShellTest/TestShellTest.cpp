e96c9f4e9209a51dab49ef468852fb6eca4b7f52
jiyeon <eren08@naver.com>
2024-07-09 09:50:15 +0900

    [feat] execute fail test case

--- TestShellTest/TestShellTest.cpp
+++ TestShellTest/TestShellTest.cpp
@@ -1,9 +1,24 @@
+#include <string>
 #include "gtest/gtest.h"
 #include "gmock/gmock.h"
-
 #include "../TestShell/TestShell.cpp"
 
-TEST(TestCaseName, TestName) {
-	EXPECT_EQ(1, 1);
-	EXPECT_TRUE(true);
+using namespace std;
+
+TEST(ShellTest, execute_test_invalid_command) {
+	TestShell shell;
+	std::string str{ "writ 3 0xAAAABBBB" };
+	EXPECT_THROW(shell.execute(str), runtime_error);
+}
+
+TEST(ShellTest, execute_test_invalid_argument1) {
+	TestShell shell;
+	std::string str{ "read 3 0xAAAABBBB00" };
+	EXPECT_THROW(shell.execute(str), runtime_error);
+}
+
+TEST(ShellTest, execute_test_invalid_argument2) {
+	TestShell shell;
+	std::string str{ "read 100 0xAAAABBBB" };
+	EXPECT_THROW(shell.execute(str), runtime_error);
 }
