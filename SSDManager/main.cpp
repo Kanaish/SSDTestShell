69276284bb7e7ebd9affe7e9ea6ae2b7a539b807
bsjn16sw <bsjn16sw@gmail.com>
2024-07-09 15:07:11 +0900

    [feat] process argc, argv in SSD main

--- SSDManager/main.cpp
+++ SSDManager/main.cpp
@@ -1,4 +1,14 @@
-int main()
-{
-	return 0;
+/* Copyright 2024 Code Love you */
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include "SSDManager.cpp"
+
+int main(int argc, char** argv) {
+    SSDManager ssd(argc, argv);
+
+    //bool successFlag = ssd.executeCommand();
+
+    return 0;
 }
