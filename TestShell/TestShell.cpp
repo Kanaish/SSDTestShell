3eba524782eabdf6de067011f23043d009bfc15c
Jongin Kim <k.jongin@gmail.com>
2024-07-09 09:08:51 +0900

    [feat] implement function prototype for TestShell

---
+++ TestShell/TestShell.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+
+#include "TestShell.h"
+
+using namespace std;
+
+void TestShell::run(void)
+{
+}
+
+bool TestShell::isValidCommand(string cmd)
+{
+	return true;
+}
+
+void TestShell::execute(string input_str)
+{
+}
+
+bool TestShell::write(string cmd)
+{
+	return true;
+}
+
+bool TestShell::read(string cmd)
+{
+	return true;
+}
+
+bool TestShell::exit(void)
+{
+	return true;
+}
+
+bool TestShell::help(void)
+{
+	return true;
+}
+
+bool TestShell::fullWrite(string cmd)
+{
+	return true;
+}
+
+bool TestShell::fullRead(void)
+{
+	return true;
+}
