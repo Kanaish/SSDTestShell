43d288fff3ccea67103c057b76f3b2b35fa92595
bsjn16sw <bsjn16sw@gmail.com>
2024-07-10 19:49:34 +0900

    [feat] update skeleton of CommandBuffer.cpp

---
+++ SSDManager/CommandBuffer.cpp
@@ -0,0 +1,37 @@
+#include "CommandBuffer.h"
+
+CommandBuffer::CommandBuffer() {
+
+}
+
+bool CommandBuffer::updateBuffer(BufferData data) {
+
+}
+
+bool CommandBuffer::isFullBuffer() {
+
+}
+
+std::vector<BufferData> CommandBuffer::flushBuffer() {
+
+}
+
+std::string CommandBuffer::findMatchedWrite(int index) {
+
+}
+
+bool CommandBuffer::createBufferFile() {
+
+}
+
+bool CommandBuffer::readBufferFile() {
+
+}
+
+bool CommandBuffer::writeBufferFile() {
+
+}
+
+bool CommandBuffer::flushBufferFile() {
+
+}
