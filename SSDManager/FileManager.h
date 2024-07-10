57a443d07219cb6c126b44487062888025cb3c63
jiyeon <eren08@naver.com>
2024-07-10 12:10:47 +0900

    [feat] implement testApp1, testApp2

--- SSDManager/FileManager.h
+++ SSDManager/FileManager.h
@@ -11,6 +11,7 @@ public:
     std::string read(std::string name, int index);
     bool write(std::string name, int index, std::string value);
     bool write(std::string name, std::string value);
+    std::string read(std::string name);
 private:
     const std::string EMPTY = "0x00000000";
     const int VALUE_LEN = 10;
--- TestShellTest/TestShellTest.cpp
+++ TestShellTest/TestShellTest.cpp
@@ -17,6 +17,7 @@ public:
     MOCK_METHOD(int, fullWrite, (const string& arg), (override));
     MOCK_METHOD(int, fullRead, (), (override));
     MOCK_METHOD(int, testApp1, (), (override));
+    MOCK_METHOD(int, testApp2, (), (override));
 };
 
 class TestShellFixture : public Test {
@@ -52,8 +53,16 @@ protected:
     }
 };
 
-TEST_F(TestShellFixture, execute_invalid_command) {
+TEST_F(TestShellFixture, execute_invalid_command1) {
     expectInvalidArgumentForWrite("writ 3 0xAAAABBBB");
+}
+
+TEST_F(TestShellFixture, execute_invalid_command2) {
+    expectInvalidArgumentForWrite("writ 3 0xAAAABBBB fff");
+}
+
+TEST_F(TestShellFixture, execute_invalid_command3) {
+    expectInvalidArgumentForWrite("exit 4");
 }
 
 TEST_F(TestShellFixture, execute_write) {
@@ -112,8 +121,19 @@ TEST_F(TestShellFixture, fullRead_pass) {
 
 TEST_F(TestShellFixture, testApp1_pass) {
     EXPECT_CALL(mock, fullWrite("0xAAAABBBB")).Times(1)
-        .WillOnce(Return(true));
+        .WillOnce(Return(0));
     EXPECT_CALL(mock, fullRead()).Times(1)
-        .WillOnce(Return(true));
-    mock.execute("testapp1");
+        .WillOnce(Return(0));
+    EXPECT_EQ(mock.TestShell::testApp1(), 0);
+}
+
+TEST_F(TestShellFixture, testApp2_pass) {
+    EXPECT_CALL(mock, write(_))
+        .Times(36)  
+        .WillRepeatedly(Return(0));
+    EXPECT_CALL(mock, read(_))
+        .Times(6)
+        .WillRepeatedly(Return(0));
+
+    EXPECT_EQ(mock.testApp2(), 0);
 }
