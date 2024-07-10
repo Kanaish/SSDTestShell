b867c43e9c73e6002cd4c81a273dd1231e576bf6
cwon.lee <wonmyth@naver.com>
2024-07-10 23:41:47 +0900

    (feat) implement createBufferFile() and readBufferFile()

--- SSDManager/CommandBuffer.cpp
+++ SSDManager/CommandBuffer.cpp
@@ -1,37 +1,103 @@
+/* Copyright 2024 Code Love you */
+#include <iostream>
+#include <fstream>
+#include <sstream>
 #include "CommandBuffer.h"
 
 CommandBuffer::CommandBuffer() {
-
+	createBufferFile();
 }
 
 bool CommandBuffer::updateBuffer(BufferData data) {
+    if (readBufferFile() == false) {
+        return false;
+    }
 
+    return true;
 }
 
 bool CommandBuffer::isFullBuffer() {
-
+    return true;
 }
 
 std::vector<BufferData> CommandBuffer::flushBuffer() {
-
+    std::vector<BufferData> data;
+    return data;
 }
 
 std::string CommandBuffer::findMatchedWrite(int index) {
-
+    return "";
 }
 
 bool CommandBuffer::createBufferFile() {
-
+    std::fstream bufferFile(BUFFER_FILE_NAME);
+    if (!bufferFile) {
+        std::ofstream ofs(BUFFER_FILE_NAME);
+        if (!ofs)
+        {
+            return false;
+        }
+    }
+    return true;
 }
 
 bool CommandBuffer::readBufferFile() {
+    std::fstream bufferFile(BUFFER_FILE_NAME);
+    if (!bufferFile) {
+        return false;
+    }
 
+    /* read nand and store to the buffer */
+    std::stringstream buffer;
+    std::string cmd;
+    buffer << bufferFile.rdbuf();
+    
+    std::string token;
+    BufferData buf_data;
+
+    // 구분자로 쉼표(,)를 사용하여 문자열 추출
+    while (std::getline(buffer, cmd, DELIMETER_STRING)) {
+        buf_data = {};
+
+        if (cmd[0] == 'W') {
+            std::stringstream cmd_stream;
+            cmd_stream << cmd;
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.cmd = 'W';
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.index = stoi(token);
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.write_value = token;
+
+            data.push_back(buf_data);
+        }
+        else if (cmd[0] == 'E') {
+            std::stringstream cmd_stream;
+            cmd_stream << cmd;
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.cmd = 'E';
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.index = stoi(token);
+
+            std::getline(cmd_stream, token, SPACE_STRING);
+            buf_data.erase_size = stoi(token);
+
+            data.push_back(buf_data);
+        }
+
+    }
+    return true;
 }
 
 bool CommandBuffer::writeBufferFile() {
-
+    return true;
 }
 
 bool CommandBuffer::flushBufferFile() {
-
+    return true;
 }
--- SSDManager/CommandBuffer.h
+++ SSDManager/CommandBuffer.h
@@ -21,11 +21,11 @@ public:
     bool isFullBuffer();
     std::vector<BufferData> flushBuffer(); // data 전달 + 파일 비우기 (flushBufferFile() 호출)
     std::string findMatchedWrite(int index); // 있으면 value 값, 없으면 ""
+
     const std::string BUFFER_FILE_NAME = "../../resources/buffer.txt";
+    // const std::string BUFFER_FILE_NAME = "../resources/buffer.txt";
 
 private:
-    std::vector<BufferData> data;
-
     // 파일 뭔가 문제 생기면 (null을 리턴? exception이 떨어지는지?) 여기서 return false;
     bool createBufferFile(); // buffer.txt가 없으면 생성 (춘원님 코드 바로 넣기)
     bool readBufferFile(); // buffer.txt를 읽어서 data에 넣기 (delimeter: ,)
@@ -33,4 +33,8 @@ private:
     bool flushBufferFile(); // buffer.txt 파일 비우기
 
     // 최적화 알고리즘 함수들 넣기
+
+    std::vector<BufferData> data;
+    const char DELIMETER_STRING = ','; // "W 1 0x12345678,E 10 5,W 2 0x12345678,"
+    const char SPACE_STRING = ' ';
 };
