8e6253ab8bdfc0ec4a583ab75a2fb3e8ba631b35
bsjn16sw <bsjn16sw@gmail.com>
2024-07-11 21:10:25 +0900

    [fix] write to file immediately (not check optimization) when buffer is empty

--- SSDManager/CommandBuffer.cpp
+++ SSDManager/CommandBuffer.cpp
@@ -14,6 +14,7 @@ bool CommandBuffer::updateBuffer(BufferData new_data) {
     if (data.empty()) {
         LOG("Buffer is clean. No need to optimize.");
         data.push_back(new_data);
+        return writeBufferFile();
     }
 
     if (new_data.cmd == 'W') {
